413c571809c388f1e081f86e6e7f07d2e1c6fc5d
Phionanamugga <113381263+Phionanamugga@users.noreply.github.com>
2023-08-12 14:44:54 +0300

    shows how to add an i.d and name to a user

--- index.py
+++ index.py
@@ -1,7 +1,16 @@
-# Create objects using classes
-
-#car = CarBlueprint()
+#shows how to create a class
+class CarBlueprint:
+    pass
+   # car = CarBlueprint() # Create objects using classes
 
 from turtle import Turtle
 timmy = Turtle()
 print(timmy)
+
+# creating a class called user
+class User():
+    pass
+user_1 = User()
+user_1.id ="001"
+user_1.username = "Angella"
+print(user_1.username)
